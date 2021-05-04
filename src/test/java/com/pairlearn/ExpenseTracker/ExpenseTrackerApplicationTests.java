9760acd3c3592ab806ca48ea6fb98730598aa8d8
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-05-05 00:57:16 +0530

    Init

---
+++ src/test/java/com/pairlearn/ExpenseTracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pairlearn.ExpenseTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
